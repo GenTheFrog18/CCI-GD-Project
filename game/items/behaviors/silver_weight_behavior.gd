c4c8d8bdb3590ba9eef0987f8e71f047c876f906
GenTheFrog18 <GenTheFrog18@proton.me>
2026-08-20 07:30:36 +0700

    fix: defer silver weight hitbox update

--- game/items/behaviors/silver_weight_behavior.gd
+++ game/items/behaviors/silver_weight_behavior.gd
@@ -11,7 +11,7 @@ func on_impact(thrown_item: Node2D, impact: ImpactData) -> ItemActionResult:
 		body.apply_damage(DamageInfo.new(impact_damage, impact.source_actor, impact.source_species_id))
 	if thrown_item.definition.item_id == &"silver_weight":
 		thrown_item.definition = ContentCatalog.get_item(&"silver_weight_impact_damaged")
-		thrown_item._apply_world_hitbox()
+		thrown_item.call_deferred(&"_apply_world_hitbox")
 		thrown_item._apply_visual()
 	elif thrown_item.definition.item_id == &"silver_weight_damaged":
 		return ItemActionResult.completed()
