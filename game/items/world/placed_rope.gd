59ded7859006c486b0bbaa4a069db7aee7e609cf
GenTheFrog18 <GenTheFrog18@proton.me>
2026-08-29 01:19:50 +0700

    feat(rope): enable map placement

--- game/items/world/placed_rope.gd
+++ game/items/world/placed_rope.gd
@@ -1,3 +1,4 @@
+@tool
 class_name PlacedRope
 extends Area2D
 
@@ -6,20 +7,33 @@ const END_TEXTURE := preload("res://assets/art/items/rope/rope_end.png")
 const ROPE_SCENE_PATH := "res://game/items/world/placed_rope.tscn"
 
 @export var persistent_id := ""
-var rope_length := 160.0
+@export_range(16.0, 1000.0, 16.0) var rope_length: float = 160.0:
+	set(value):
+		rope_length = maxf(16.0, floorf(float(value) / 16.0) * 16.0)
+		if Engine.is_editor_hint() and is_inside_tree():
+			_rebuild_rope()
+
 var chain_root: PlacedRope
 var _anchor := Vector2.ZERO
 var _end_sprite: Sprite2D
+var _configured := false
 
 func configure(anchor_position: Vector2, length: float, root: PlacedRope = null) -> void:
+	_configured = true
 	_anchor = anchor_position
 	rope_length = maxf(16.0, floorf(length / 16.0) * 16.0)
 	chain_root = root
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		chain_root = self
+		_anchor = global_position
+		_build_rope()
+		return
 	if chain_root == null:
 		chain_root = self
-	global_position = _anchor
+	if _configured:
+		global_position = _anchor
 	add_to_group(&"placed_ropes")
 	if get_chain_root() == self:
 		if persistent_id.is_empty():
@@ -70,6 +84,7 @@ func restore_state(data: Dictionary) -> void:
 	global_position = Vector2(float(saved_position[0]), float(saved_position[1]))
 	_anchor = global_position
 	chain_root = self
+	_configured = true
 	var saved_lengths: Array = data.get("segment_lengths", [rope_length])
 	if saved_lengths.is_empty():
 		saved_lengths = [rope_length]
